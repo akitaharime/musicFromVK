84af72ab25898bfcd881ca492470ac1c50d43e69
akitaharime <110650206+akitaharime@users.noreply.github.com>
2022-09-13 20:03:54 +1000

    Create config.ts

---
+++ config.ts
@@ -0,0 +1,8 @@
+import { Config } from './config.type'
+
+export const config: Config = {
+    api_id: 9824217, //API Id приложения MTProto
+    api_hash: 'f80ba4196a4b62feb9c80e84b46f2a2b', //API Hash приложения MTProto
+    tg_token: '5541508025:AAEHKwjI7M--OqLrUGWzleT_OYOZ5zcEQl4', //Токен от телеграм бота
+    vk_token: '6be59d30e6b346f17c4ca695df004f228379b78c261b0468b55da6f6dbfff5468beffa5c1dae4c0009ffa', //Токен от аккаунта вк
+    uploadThreads: 3 //Сколько одновременно частей загружать на сервер телеграм
